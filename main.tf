1e6805611a37e03ebf440f8d40ad26ccbe3a0f5f
clanda20 <clanda@hotmail.com>
2024-02-28 23:00:14 -0800

    First Commit

---
+++ main.tf
@@ -0,0 +1,97 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+
+variable "vpc_id" {
+  default = "vpc-0c4a84b487dfdeb5b"
+}
+
+resource "aws_security_group" "drines_ucsc_ssh" {
+  name = "christian_test_ec2_ssh"
+  vpc_id = var.vpc_id
+  
+  tags = {
+    Name = "SEQAX409"
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["99.104.37.103/32"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "drines_ucsc_app" {
+  name = "christian_test_ec2_flask_app"
+  vpc_id = var.vpc_id
+  
+  tags = {
+    Name = "SEQAX409"
+  }
+
+  ingress {
+    from_port = 5000
+    to_port = 5000
+    protocol = "tcp"
+    cidr_blocks = ["99.104.37.103/32"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["my_first_ami_*"]
+  }
+
+  owners = ["145067424316"]
+}
+
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.drines_ucsc_ssh.id, aws_security_group.drines_ucsc_app.id]
+  key_name = "testing_aws_key"
+
+  count = 2
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+output "hostid" {
+  value = aws_instance.app_server.*.public_dns
+}
---
+++ packer_with_provisioner.pkr.hcl
@@ -0,0 +1,69 @@
+packer {
+  required_plugins {
+    amazon = {
+      version = ">= 1.2.8"
+      source  = "github.com/hashicorp/amazon"
+    }
+  }
+}
+
+locals {
+  timestamp = regex_replace(timestamp(), "[- TZ:]", "")
+}
+
+source "amazon-ebs" "ubuntu" {
+  ##ami_name      = "my_first_ami_{{isotime '2006.01.02.15:04:05'}}"
+  ami_name = "my_first_ami_${local.timestamp}"
+  instance_type = "t2.micro"
+  region        = "${var.my_region}"
+  source_ami_filter {
+    filters = {
+      name                = "${var.source_ami_name}"
+      root-device-type    = "ebs"
+      virtualization-type = "hvm"
+    }
+    most_recent = true
+    owners      = ["099720109477"]
+  }
+  ssh_username = "ubuntu"
+}
+
+build {
+  name    = "learn-packer"
+  sources = [
+    "source.amazon-ebs.ubuntu"
+  ]
+
+  provisioner "file" {
+    source = "app_files"
+    destination = "/home/ubuntu/"
+  }
+
+  provisioner "shell" {
+    inline = [
+      "ls /home/ubuntu",
+      "echo 'Install packages with apt'",
+      "sudo apt update -y",
+      "sudo apt install python3-pip -y",
+      "sudo apt install gcc -y",
+      "sudo snap install redis",
+      "sudo gcc -o dummyserv dummy_serv.c",
+      "echo 'Setup the my app service with system'",
+      "sudo cp /home/ubuntu/app_files/myapp.service /etc/systemd/system/myapp.service",
+      "sudo systemctl enable myapp",
+    ]
+  }
+
+  
+
+}
+
+variable "my_region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "source_ami_name" {
+  type = string
+  default = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"
+}
